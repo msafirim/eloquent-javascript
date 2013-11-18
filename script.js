2b8210b8ad2eb7341e88b789f797984462951c48
Moses Msafiri <mssmsfr@gmail.com>
2013-11-18 23:55:03 +0300

    Use while to a program which prompt user

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-for (var current = 20; ; current++) {
-  if (current % 7 == 0)
+var answer;
+while (true) {
+  answer = prompt("You! What is the value of 2 + 2?", "");
+  if (answer == "4") {
+    alert("You must be a genius or something.");
     break;
+  }
+  else if (answer == "3" || answer == "5") {
+    alert("Almost!");
+  }
+  else {
+    alert("You're an embarrassment.");
+  }
 }
-alert(current);
