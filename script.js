9b6caee677052a669b05306e663b41a89bd5064b
Moses Msafiri <mssmsfr@gmail.com>
2013-11-19 00:23:08 +0300

    Add a prompt with a fallback value

--- script.js
+++ script.js
@@ -1,14 +1,2 @@
-var answer;
-while (true) {
-  answer = prompt("You! What is the value of 2 + 2?", "");
-  if (answer == "4") {
-    alert("You must be a genius or something.");
-    break;
-  }
-  else if (answer == "3" || answer == "5") {
-    alert("Almost!");
-  }
-  else {
-    alert("You're an embarrassment.");
-  }
-}
+var input = prompt("What is your name?", "Kilgore Trout");
+alert("Well hello " + (input || "dear"));
