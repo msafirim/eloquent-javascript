e42e6c54fbc5606b96b8a3f47beb83a8fa7dcbd0
Moses Msafiri <mssmsfr@gmail.com>
2013-11-18 23:30:08 +0300

    Use else statement

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 for (var counter = 0; counter < 20; counter++) {
-  if (counter % 3 == 0 && counter % 4 == 0)
+  if (counter % 4 == 0)
     alert(counter);
+  else
+    alert("(" + counter + ")");
 }
