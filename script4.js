f4bba344150eea10675a7a210283b444f4003b79
Moses Msafiri <mssmsfr@gmail.com>
2013-11-20 11:12:57 +0300

    Access an object

--- script4.js
+++ script4.js
@@ -1,3 +1,10 @@
 var text = "purple haze";
 console.log(text["length"]);
 console.log(text.length);
+
+var cat = {colour: "grey", name: "Spot", size: 46};
+cat.size = 47;
+console.log(cat.size);
+delete cat.size;
+console.log(cat.size);
+console.log(cat);
