0f92bc536142dac1f4f07fd097ea3a3a357978f9
Moses Msafiri <mssmsfr@gmail.com>
2013-11-20 09:28:16 +0300

    Add a puzzle to show recursion

--- script.js
+++ script.js
@@ -9,10 +9,17 @@ var addTwo = makeAddFunction(2);
 var addFive = makeAddFunction(5);
 console.log(addTwo(1) + addFive(1));
 
-function chicken() {
-  return egg();
+function findSequence(goal) {
+  function find(start, history) {
+    if (start == goal)
+      return history;
+    else if (start > goal)
+      return null;
+    else
+      return find(start + 5, "(" + history + " + 5)") ||
+             find(start * 3, "(" + history + " * 3)");
+  }
+  return find(1, "1");
 }
-function egg() {
-  return chicken();
-}
-console.log(chicken() + " came first.");
+
+console.log(findSequence(24));
