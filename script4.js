9ac2ee28d7c84938a0a01d13d4c0b1ba52dc7766
Moses Msafiri <mssmsfr@gmail.com>
2013-11-20 12:01:39 +0300

    Set property name using a value

--- script4.js
+++ script4.js
@@ -27,3 +27,13 @@ var chineseBox = {};
 chineseBox.content = chineseBox;
 console.log("content" in chineseBox);
 console.log("content" in chineseBox.content);
+
+console.log(chineseBox);
+
+var set = {"Spot": true};
+// Add "White Fang" to the set
+set["White Fang"] = true;
+// Remove "Spot"
+delete set["Spot"];
+// See if "Asoka" is in the set
+console.log("Asoka" in set);
