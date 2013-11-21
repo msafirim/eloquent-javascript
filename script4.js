c14daa6f44fcadfb67c2d5af20f61a64ba099733
Moses Msafiri <mssmsfr@gmail.com>
2013-11-21 14:37:55 +0300

    Add function to find the oldest living cat

--- script4.js
+++ script4.js
@@ -272,3 +272,22 @@ function formatDate(date) {
              "/" + date.getFullYear();
 }
 console.log(formatDate(new Date(2000, 0, 1)));
+
+
+function oldestCat(data) {
+  var oldest = null;
+
+  for (var name in data) {
+    var cat = data[name];
+    if (!("death" in cat) &&
+        (oldest == null || oldest.birth > cat.birth))
+      oldest = cat;
+  }
+
+  if (oldest == null)
+    return null;
+  else
+    return oldest.name;
+}
+
+console.log(oldestCat(catData));
