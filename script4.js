ac7b73b9cf7dc2d3e73c65a16ddc85dc3a8aaebd
Moses Msafiri <mssmsfr@gmail.com>
2013-11-21 16:21:21 +0300

    Modify range function

--- script4.js
+++ script4.js
@@ -307,3 +307,18 @@ function add(number, howmuch) {
 
 console.log(add(6));
 console.log(add(6, 4));
+
+
+function range(start, end) {
+  if (arguments.length < 2) {
+    end = start;
+    start = 0;
+  }
+  var result = [];
+  for (var i = start; i <= end; i++)
+    result.push(i);
+  return result;
+}
+
+console.log(range(4));
+console.log(range(2, 4));
