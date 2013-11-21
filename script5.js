1ac065acee73cef5a7d3cae8d40d57e5d5906ed9
Moses Msafiri <mssmsfr@gmail.com>
2013-11-21 18:54:20 +0300

    Add function to find string between string to check for errors

--- script5.js
+++ script5.js
@@ -5,3 +5,9 @@ function power(base, exponent) {
   return result;
 }
 
+
+function between(string, start, end) {
+  var startAt = string.indexOf(start) + start.length;
+  var endAt = string.indexOf(end, startAt);
+  return string.slice(startAt, endAt);
+}
