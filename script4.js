2651fce237c0ac097fce8c1ffc66f48e033fdf54
Moses Msafiri <mssmsfr@gmail.com>
2013-11-21 14:23:01 +0300

    Add function to add zero before day and month

--- script4.js
+++ script4.js
@@ -259,3 +259,16 @@ function catInfo(data, name) {
 }
 
 console.log(catInfo(catData, "Fat Igor"));
+
+
+function formatDate(date) {
+  function pad(number) {
+    if (number < 10)
+      return "0" + number;
+    else
+      return number;
+  }
+  return pad(date.getDate()) + "/" + pad(date.getMonth() + 1) +
+             "/" + date.getFullYear();
+}
+console.log(formatDate(new Date(2000, 0, 1)));
