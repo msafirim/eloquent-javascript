41026fbff788e5e10dbc790b27bf93ac39a2cd8c
Moses Msafiri <mssmsfr@gmail.com>
2013-11-23 22:05:29 +0300

    Add sum function which use forEach

--- script6.js
+++ script6.js
@@ -2,3 +2,13 @@ function printArray(array) {
   for (var i = 0; i < array.length; i++)
     console.log(array[i]);
 }
+
+
+function sum(numbers) {
+  var total = 0;
+  forEach(numbers, function (number) {
+    total += number;
+  });
+  return total;
+}
+console.log(sum([1, 10, 100]));
