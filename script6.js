b284356fcb73dd561a8141391ae5ff69deb4cb64
Moses Msafiri <mssmsfr@gmail.com>
2013-11-24 21:48:43 +0300

    Add function which map/build array from return statement

--- script6.js
+++ script6.js
@@ -77,3 +77,14 @@ function equals(x) {
 function countZeroes(array) {
   return count(equals(0), array);
 }
+
+
+function map(func, array) {
+  var result = [];
+  forEach(array, function (element) {
+    result.push(func(element));
+  });
+  return result;
+}
+
+console.log(map(Math.round, [0.01, 2, 9.89, Math.PI]));
