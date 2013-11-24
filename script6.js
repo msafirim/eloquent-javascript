280a79bfa7d1b0130425c42a9dd05d67b2ddf475
Moses Msafiri <mssmsfr@gmail.com>
2013-11-24 12:40:42 +0300

    Add reduce function which combines array

--- script6.js
+++ script6.js
@@ -38,3 +38,19 @@ function negate(func) {
     return !func.apply(null, arguments);
   };
 }
+
+
+function reduce(combine, base, array) {
+  forEach(array, function (element) {
+    base = combine(base, element);
+  });
+  return base;
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+function sum(numbers) {
+  return reduce(add, 0, numbers);
+}
