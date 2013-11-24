34d45849de4922abc43d430360c3d9b0275dcfb8
Moses Msafiri <mssmsfr@gmail.com>
2013-11-24 21:43:08 +0300

    Add count function with function to count zeros

--- script6.js
+++ script6.js
@@ -62,3 +62,18 @@ function countZeroes(array) {
   }
   return reduce(counter, 0, array);
 }
+
+
+function count(test, array) {
+  return reduce(function(total, element) {
+    return total + (test(element) ? 1 : 0);
+  }, 0, array);
+}
+
+function equals(x) {
+  return function(element) {return x === element;};
+}
+
+function countZeroes(array) {
+  return count(equals(0), array);
+}
