4ec612e981ab6be3baad86f78edc7d52d90fae46
Moses Msafiri <mssmsfr@gmail.com>
2013-11-25 01:00:21 +0300

    Add negate function which is function composition

--- script6.js
+++ script6.js
@@ -335,3 +335,10 @@ console.log(map(partial(op["+"], 1), [0, 2, 4, 6, 8, 10]));
 function square(x) {return x * x;}
 
 console.log(map(partial(map, square), [[10, 100], [12, 16], [0, 1]]));
+
+
+function negate(func) {
+  return function() {
+    return !func.apply(null, arguments);
+  };
+}
