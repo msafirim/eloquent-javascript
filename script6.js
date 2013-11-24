29f05f35cb965c3e93d557a1a7e4007de1dcf4d6
Moses Msafiri <mssmsfr@gmail.com>
2013-11-24 23:14:12 +0300

    Add function to process paragraphs

--- script6.js
+++ script6.js
@@ -88,3 +88,20 @@ function map(func, array) {
 }
 
 console.log(map(Math.round, [0.01, 2, 9.89, Math.PI]));
+
+
+function processParagraph(paragraph) {
+  var header = 0;
+  while (paragraph.charAt(0) == "%") {
+    paragraph = paragraph.slice(1);
+    header++;
+  }
+
+  return {type: (header == 0 ? "p" : "h" + header),
+          content: paragraph};
+}
+
+console.log(processParagraph(paragraphs[0]));
+
+var paragraphs = map(processParagraph,
+                     recluseFile().split("\n\n"));
