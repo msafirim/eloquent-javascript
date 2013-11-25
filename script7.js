251d0ff9974f79ecc7a9ca192f8bcfe6ee8aef74
Moses Msafiri <mssmsfr@gmail.com>
2013-11-25 19:43:48 +0300

    Add function possibleDirections

--- script7.js
+++ script7.js
@@ -229,3 +229,20 @@ function samePoint(a, b) {
 
 console.log(samePoint(addPoints(point(10, 10), point(4, -2)),
                point(14, 8)));
+
+
+function possibleDirections(from) {
+  var mapSize = 20;
+  function insideMap(point) {
+    return point.x >= 0 && point.x < mapSize &&
+           point.y >= 0 && point.y < mapSize;
+  }
+
+  var directions = [point(-1, 0), point(1, 0), point(0, -1),
+                    point(0, 1), point(-1, -1), point(-1, 1),
+                    point(1, 1), point(1, -1)];
+  return filter(insideMap, map(partial(addPoints, from),
+                               directions));
+}
+
+console.log(possibleDirections(point(0, 0)));
