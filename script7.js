a4da2dddccaf72e70fe2b90d8d21dad929daa0bc
Moses Msafiri <mssmsfr@gmail.com>
2013-11-25 19:48:56 +0300

    Add function estimateDistance

--- script7.js
+++ script7.js
@@ -246,3 +246,13 @@ function possibleDirections(from) {
 }
 
 console.log(possibleDirections(point(0, 0)));
+
+
+function estimatedDistance(pointA, pointB) {
+  var dx = Math.abs(pointA.x - pointB.x),
+      dy = Math.abs(pointA.y - pointB.y);
+  if (dx > dy)
+    return (dx - dy) * 100 + dy * 141;
+  else
+    return (dy - dx) * 100 + dx * 141;
+}
