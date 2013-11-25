47ba5ccfa4e1390e1d454691e11837c89c2d9b98
Moses Msafiri <mssmsfr@gmail.com>
2013-11-25 21:08:39 +0300

    Add function rabbit with new variable

--- script8.js
+++ script8.js
@@ -20,3 +20,14 @@ speak.apply(fatRabbit, ["Yum."]);
 
 
 speak.call(fatRabbit, "Burp.");
+
+
+function Rabbit(adjective) {
+  this.adjective = adjective;
+  this.speak = function(line) {
+    console.log("The ", this.adjective, " rabbit says '", line, "'");
+  };
+}
+
+var killerRabbit = new Rabbit("killer");
+killerRabbit.speak("GRAAAAAAAAAH!");
