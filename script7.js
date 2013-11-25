d6b83e318a6dd0c6a7d862c99ae55329120b2f9c
Moses Msafiri <mssmsfr@gmail.com>
2013-11-25 13:40:24 +0300

    Add function makeRoad

--- script7.js
+++ script7.js
@@ -1,4 +1,17 @@
+/*
 var roads = {"Point Kiukiu": [{to: "Hanaiapa", distance: 19},
                               {to: "Mt Feani", distance: 15},
                               {to: "Taaoa", distance: 15}],
-             "Taaoa": [/* et cetera */]};
+             "Taaoa": [/* et cetera */ //]};
+
+
+var roads = {};
+function makeRoad(from, to, length) {
+  function addRoad(from, to) {
+    if (!(from in roads))
+      roads[from] = [];
+    roads[from].push({to: to, distance: length});
+  }
+  addRoad(from, to);
+  addRoad(to, from);
+}
