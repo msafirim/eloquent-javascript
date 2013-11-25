44bc38267c89bcdf311b74332ecfc3924588602c
Moses Msafiri <mssmsfr@gmail.com>
2013-11-25 21:00:08 +0300

    Add speak function

--- script8.js
+++ script8.js
@@ -4,3 +4,13 @@ rabbit.speak = function(line) {
 };
 
 rabbit.speak("Well, now you're asking me.");
+
+
+function speak(line) {
+  console.log("The ", this.adjective, " rabbit says '", line, "'");
+}
+var whiteRabbit = {adjective: "white", speak: speak};
+var fatRabbit = {adjective: "fat", speak: speak};
+
+whiteRabbit.speak("Oh my ears and whiskers, how late it's getting!");
+fatRabbit.speak("I could sure use a carrot right now.");
