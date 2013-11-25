69f0cb1587c1fa7a7e4234a09949be2164d7570c
Moses Msafiri <mssmsfr@gmail.com>
2013-11-25 21:02:38 +0300

    Use apply method to speak object

--- script8.js
+++ script8.js
@@ -14,3 +14,6 @@ var fatRabbit = {adjective: "fat", speak: speak};
 
 whiteRabbit.speak("Oh my ears and whiskers, how late it's getting!");
 fatRabbit.speak("I could sure use a carrot right now.");
+
+
+speak.apply(fatRabbit, ["Yum."]);
