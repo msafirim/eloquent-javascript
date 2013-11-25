5e5d0f3b9ec33a7347a56a3eccfa373fbb7c8f9d
Moses Msafiri <mssmsfr@gmail.com>
2013-11-25 16:48:49 +0300

    Add function filter

--- script7.js
+++ script7.js
@@ -131,3 +131,15 @@ function flatten(arrays) {
     });
     return result;
   }
+
+
+  function filter(test, array) {
+  var result = [];
+  forEach(array, function (element) {
+    if (test(element))
+      result.push(element);
+  });
+  return result;
+}
+
+console.log(filter(partial(op[">"], 5), [0, 4, 8, 12]));
