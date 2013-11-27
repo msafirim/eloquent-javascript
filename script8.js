bbd544d2a511e17dc9473c2e459dd34fb499319b
Moses Msafiri <mssmsfr@gmail.com>
2013-11-27 14:41:21 +0300

    Add act metthod to LichenEater

--- script8.js
+++ script8.js
@@ -580,3 +580,18 @@ LichenEater.prototype.act = function(surroundings) {
 LichenEater.prototype.character = "c";
 
 creatureTypes.register(LichenEater);
+
+
+LichenEater.prototype.act = function(surroundings) {
+  var emptySpace = findDirections(surroundings, " ");
+  var lichen = findDirections(surroundings, "*");
+
+  if (this.energy >= 30 && emptySpace.length > 0)
+    return {type: "reproduce", direction: randomElement(emptySpace)};
+  else if (lichen.length > 1)
+    return {type: "eat", direction: randomElement(lichen)};
+  else if (emptySpace.length > 0)
+    return {type: "move", direction: randomElement(emptySpace)};
+  else
+    return {type: "wait"};
+};
