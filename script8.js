8c240132f640144fa994a8d3c5da35eaf84576e9
Moses Msafiri <mssmsfr@gmail.com>
2013-11-27 14:24:21 +0300

    Add property names to Dictionary prototype

--- script8.js
+++ script8.js
@@ -437,3 +437,12 @@ BouncingBug.prototype.act = function(surroundings) {
 BouncingBug.prototype.character = "%";
 
 creatureTypes.register(BouncingBug);
+
+
+Dictionary.prototype.names = function() {
+  var names = [];
+  this.each(function(name, value) {names.push(name);});
+  return names;
+};
+
+console.log(directions.names());
