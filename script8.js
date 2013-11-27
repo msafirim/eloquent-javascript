02546342247f9c79fc36524ec343c4af3c1f9434
Moses Msafiri <mssmsfr@gmail.com>
2013-11-27 12:24:19 +0300

    Add hasOwnProperty method to rewrite properties method

--- script8.js
+++ script8.js
@@ -98,3 +98,16 @@ Object.prototype.properties = function() {
 
 var test = {x: 10, y: 3};
 console.log(test.properties());
+
+
+Object.prototype.properties = function() {
+  var result = [];
+  for (var property in this) {
+    if (this.hasOwnProperty(property))
+      result.push(property);
+  }
+  return result;
+};
+
+var test = {"Fat Igor": true, "Fireball": true};
+console.log(test.properties());
