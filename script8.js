3aeeddcc8f21fba61f22c161947815fb9fc1cb57
Moses Msafiri <mssmsfr@gmail.com>
2013-11-27 13:53:25 +0300

    Add method toString to the Terrarium prototype

--- script8.js
+++ script8.js
@@ -292,3 +292,16 @@ function characterFromElement(element) {
 }
 
 console.log(characterFromElement(wall));
+
+
+Terrarium.prototype.toString = function() {
+  var characters = [];
+  var endOfLine = this.grid.width - 1;
+  this.grid.each(function(point, value) {
+    characters.push(characterFromElement(value));
+    if (point.x == endOfLine)
+      characters.push("\n");
+  });
+  return characters.join("");
+};
+
