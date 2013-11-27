3d3f6fdc2045afd70d568b5ca4c5675a256b5145
Moses Msafiri <mssmsfr@gmail.com>
2013-11-27 15:25:01 +0300

    Inherit prototype of an item

--- script8.js
+++ script8.js
@@ -682,3 +682,19 @@ var Item = {
 
 var lantern = Item.create("the brass lantern");
 lantern.kick();
+
+
+var DetailedItem = Item.extend({
+  construct: function(name, details) {
+    Item.construct.call(this, name);
+    this.details = details;
+  },
+  inspect: function() {
+    console.log("you see ", this.name, ", ", this.details, ".");
+  }
+});
+
+var giantSloth = DetailedItem.create(
+  "the giant sloth",
+  "it is quietly hanging from a tree, munching leaves");
+giantSloth.inspect();
