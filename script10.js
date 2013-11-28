1fac4573acd1e3ed364f1f0ff3a1d1b4ba117295
Moses Msafiri <mssmsfr@gmail.com>
2013-11-28 13:55:51 +0300

    Add extractDate function

--- script10.js
+++ script10.js
@@ -59,3 +59,14 @@ console.log("Giant Ape".match(/giant (\w+)/i));
 var parenthesized = prompt("Tell me something", "").match(/\((.*)\)/);
 if (parenthesized != null)
   console.log("You parenthesized '", parenthesized[1], "'");
+
+
+function extractDate(string) {
+  var found = string.match(/(\d\d?)\/(\d\d?)\/(\d{4})/);
+  if (found == null)
+    throw new Error("No date found in '" + string + "'.");
+  return new Date(Number(found[3]), Number(found[2]) - 1,
+                  Number(found[1]));
+}
+
+console.log(extractDate("born 5/2/2007 (mother Noog): Long-ear Johnson"));
