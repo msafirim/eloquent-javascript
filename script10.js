e17122e95181301216ab468bf21704e0575863cf
Moses Msafiri <mssmsfr@gmail.com>
2013-11-28 12:47:24 +0300

    Show how to search asterisk or brace in a string

--- script10.js
+++ script10.js
@@ -1,2 +1,8 @@
 var slash = /\//;
 console.log("AC/DC".search(slash));
+
+
+var asteriskOrBrace = /[\{\*]/;
+var story =
+  "We noticed the *giant sloth*, hanging from a giant branch.";
+console.log(story.search(asteriskOrBrace));
