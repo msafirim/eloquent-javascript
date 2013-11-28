6381b28b0487b96780ac151016e9ef68e32363c1
Moses Msafiri <mssmsfr@gmail.com>
2013-11-28 12:55:01 +0300

    Show to escape digit character

--- script10.js
+++ script10.js
@@ -6,3 +6,7 @@ var asteriskOrBrace = /[\{\*]/;
 var story =
   "We noticed the *giant sloth*, hanging from a giant branch.";
 console.log(story.search(asteriskOrBrace));
+
+
+var digitSurroundedBySpace = /\s\d\s/;
+console.log("1a 2 3d".search(digitSurroundedBySpace));
