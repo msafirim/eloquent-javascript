91a79ff0b605adebc8a5ad00efbac1603016bf7d
Moses Msafiri <mssmsfr@gmail.com>
2013-11-28 13:38:35 +0300

    Add to show pattern matches email addresses

--- script10.js
+++ script10.js
@@ -34,3 +34,10 @@ console.log("Its (the sloth's) claws were gigantic!".search(parenthesizedText));
 
 var datePattern = /\d{1,2}\/\d\d?\/\d{4}/;
 console.log("born 15/11/2003 (mother Spot): White Fang".search(datePattern));
+
+
+var mailAddress = /\b[\w\.-]+@[\w\.-]+\.\w{2,3}\b/;
+
+console.log(mailAddress.test("kenny@test.net"));
+console.log(mailAddress.test("I mailt kenny@tets.nets, but it didn wrok!"));
+console.log(mailAddress.test("the_giant_sloth@gmail.com"));
