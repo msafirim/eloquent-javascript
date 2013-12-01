a5e47e88b2f7d343851a02fe7c08e9cdd3e9dc0b
Moses Msafiri <mssmsfr@gmail.com>
2013-12-01 13:06:52 +0300

    Add form validation function

--- script11.js
+++ script11.js
@@ -13,5 +13,31 @@ console.log(decodeURIComponent(encoded));
 console.log(document.location.href);
 console.log(document.title);
 
-var form = window.open("example_getinfo.html");
+var form = window.open("myfirstpage.html");
 console.log(form);
+
+
+var userForm = document.forms.userinfo;
+console.log(userForm.method);
+console.log(userForm.action);
+
+
+
+function validInfo(form) {
+  return form.elements.name.value != "" && /^.+@.+\.\w{2,3}$/.test(form.elements.email.value);
+}
+
+console.log(validInfo(document.forms.userinfo));
+
+
+userForm.elements.send.onclick = function() {
+  alert("Click.");
+};
+
+
+userForm.elements.send.onclick = function() {
+  if (validInfo(userForm))
+    userForm.submit();
+  else
+    alert("Give us a name and a valid e-mail address!");
+};
