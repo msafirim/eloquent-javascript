01843523750acf370411a4c9e7d2013ce55642b5
Moses Msafiri <mssmsfr@gmail.com>
2013-12-01 22:36:27 +0300

    Add to show firstChild and lastChild

--- script12.js
+++ script12.js
@@ -1,3 +1,7 @@
 console.log(document.body);
-console.log(document.body.parentNode);
-console.log(document.body.childNodes.length);
+//console.log(document.body.parentNode);
+//console.log(document.body.childNodes.length);
+
+
+console.log(document.documentElement.firstChild);
+console.log(document.documentElement.lastChild);
