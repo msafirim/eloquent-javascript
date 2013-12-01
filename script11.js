29de904a254fc246244408d8dfa036804c561caa
Moses Msafiri <mssmsfr@gmail.com>
2013-12-01 11:50:48 +0300

    Add to show encoded and decode Uri component

--- script11.js
+++ script11.js
@@ -3,3 +3,8 @@ var perry = window.open("http://www.pbfcomics.com");
 console.log(perry.Math);
 
 perry.close();
+
+
+var encoded = encodeURIComponent("aztec empire");
+console.log(encoded);
+console.log(decodeURIComponent(encoded));
