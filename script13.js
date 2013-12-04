28f4cb99e6eb52f6b3ee5f5835b9e69adc81cfda
Moses Msafiri <mssmsfr@gmail.com>
2013-12-04 11:10:53 +0300

    Add function unregisterEventHandler

--- script13.js
+++ script13.js
@@ -1,4 +1,3 @@
-
 function registerEventHandler(node, event, handler) {
   if (typeof node.addEventListener == "function")
     node.addEventListener(event, handler, false);
@@ -8,3 +7,11 @@ function registerEventHandler(node, event, handler) {
 
 registerEventHandler(document.getElementById("button"), "click",
                      function(){console.log("Click (2)");});
+
+
+function unregisterEventHandler(node, event, handler) {
+  if (typeof node.removeEventListener == "function")
+    node.removeEventListener(event, handler, false);
+  else
+    node.detachEvent("on" + event, handler);
+}
